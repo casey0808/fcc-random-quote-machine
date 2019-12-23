9447a8c7f5085069bb9182a9e28568225f1723f1
casey0808 <casey_c0808@outlook.com>
2019-12-24 07:19:22 +0800

    clean code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import QuoteMachine from './components/QuoteMachine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedQuoteIndex: null,
+      quotes: []
+    }
+    this.nextQuoteClickHandler = this.nextQuoteClickHandler.bind(this);
+    this.getRandomIndex = this.getRandomIndex.bind(this);
+  }
+
+  componentDidMount(){
+    fetch("https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json")
+    .then(data => data.json())
+    .then(quotes => this.setState({quotes}, this.nextQuoteClickHandler())
+    )
+  }
+  get selectedQuote(){
+    if(!this.state.quotes.length){
+      return this.state.quotes[0];
+    }
+    return this.state.quotes[this.state.selectedQuoteIndex];
+  }
+
+  getRandomIndex() {
+    if(!this.state.quotes.length){
+      return 0;
+    }
+    return Math.floor(Math.random() * Math.floor(this.state.quotes.length));
+  }
+
+  nextQuoteClickHandler() {
+      this.setState({
+        selectedQuoteIndex: this.getRandomIndex()
+      })
+  }
+  
+
+  render(){
+    return (
+      <div className="App" id="quote-box">
+        <QuoteMachine selectedQuote={this.selectedQuote} nextQuoteClickHandler={this.nextQuoteClickHandler}/>
+      </div>
+    );
+  }
 }
 
 export default App;
